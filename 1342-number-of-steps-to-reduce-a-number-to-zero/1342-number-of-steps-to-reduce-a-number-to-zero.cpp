b51a983350f0cfd61306e0a51e0d0b7c5471a09b
ankityad971 <ankityad971@gmail.com>
2026-06-18 00:03:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (79.95%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        if (num == 0) return 0;
+        int s=0;
+        while(num>0){
+            if (num%2==0) num/=2;
+            else num -=1;
+s++;
+        }
+        return s;
 
-        int ones = __builtin_popcount(num);      // count of 1 bits
-        int bits = 31 - __builtin_clz(num);      // index of MSB
-        return bits + ones;
     }
 };
